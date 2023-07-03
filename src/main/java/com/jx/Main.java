742c7fcc6d953ca63c5c4537511385866944a5ac
jx <1925315179@qq.com>
2023-07-03 21:11:26 +0800

    213

---
+++ src/main/java/com/jx/Main.java
@@ -0,0 +1,14 @@
+package com.jx;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git2!");
+        System.out.println("Hello Git3!");
+        System.out.println("master test!");
+        System.out.println("hot-fix test!");
+        System.out.println("push test!");
+        System.out.println("pull test!");
+    }
+}
